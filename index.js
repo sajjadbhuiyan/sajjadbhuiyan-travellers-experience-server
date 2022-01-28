456d66340b00ac4feb898eeeb7d8f1045ef3a8fb
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-01-28 10:00:56 +0600

    customize all

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zl5hq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
